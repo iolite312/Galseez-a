d5c8408d4d94ffaf1fa7b5e6d99c9103d42c73cc
superman312 <68647023+superman312@users.noreply.github.com>
2023-06-04 16:29:39 +0200

    start login screen

--- client/main.js
+++ client/main.js
@@ -1,12 +1,18 @@
 import './style.scss'
 import { initializeMap } from './js/map';
-import { getCookie } from './js/cookie'
+import { getCookie } from './js/cookie';
 
 const tokenCookie = getCookie('token')
 
 if (!tokenCookie) {
     document.querySelector('#app').innerHTML = `
-        no cookie
+        <div>
+            <label for="userName">Gebruikersnaam:</label>
+            <input name='userName' type="text" id="userName">
+            <label for="password">Wachtwoord:</label>
+            <input name='password' type="text">
+            <button onclick="submitForm()">Inloggen</button>
+        </div>
     `
 } else {
     document.querySelector('#app').innerHTML = `
