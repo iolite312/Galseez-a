6eb68c7fac167f383f81fb941baa1c0d74649f8a
superman312 <68647023+superman312@users.noreply.github.com>
2023-06-04 19:21:01 +0200

    fixed a cors error when calling the backend

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ const app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser()) // for parsing cookies
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
